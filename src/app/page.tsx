dea1a2d28a4a7bfbe16d60540703c450356a55ef
Deep <110001574+MiguelDeep@users.noreply.github.com>
2025-06-12 00:16:19 +0100

    construindo a pagina inicial.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,56 @@
+import Header from "@/components/Header";
+import { Play } from "lucide-react";
+import IconNetflix from "@/images/netflix.svg";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Este é um clone da Netflix.</h1>
+    <div>
+      <div className="relative h-screen w-full overflow-hidden bg-black">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        >
+          <source src="/_Introduo_do_Meu_perfil.mp4" type="video/mp4" />
+          Seu navegador não suporta vídeos HTML5.
+        </video>
+        <div className="absolute inset-0  bg-opacity-50 z-10" />
+        <div className="absolute bottom-0 left-0 w-full h-60 bg-gradient-to-t from-black to-transparent z-20 pointer-events-none" />
+        <div className="relative z-30">
+          <Header />
+          <div className="p-8 text-white">
+            <div className="flex items-center mt-40 gap-4">
+              <Image src={IconNetflix} alt="icon Netflix" />
+              <p className="font-bold text-xl">F I L M E</p>
+            </div>
+
+            <h1 className="text-4xl font-bold mt-60 mb-4">
+              A tragédia da Oceangate
+            </h1>
+            <p className="text-lg w-96 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+              deleniti molestiae tenetur nobis iusto id necessitatibus! Aliquid
+              distinctio qui amet sunt. Fugit rerum aspernatur, alias fugiat
+              velit nostrum beatae fuga.
+            </p>
+
+            <div className="mt-4 flex gap-4">
+              <button className="bg-white flex gap-4 text-black px-6 py-2 rounded hover:bg-gray-300 transition">
+                <Play /> Ver
+              </button>
+              <button className="bg-gray-700 text-white px-6 py-2 rounded hover:bg-gray-600 transition">
+                ℹ Mais informações
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="m-10">
+        <h1 className="font-bold">Minha Lista</h1>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,67 @@
+"use client"
+import React, { ComponentProps, useEffect, useState } from "react";
+import Image from "next/image";
+import Logo from "@/images/netflix (1).svg";
+import Profile from "@/images/Captura de tela 2025-06-11 225327.png";
+import { Search, Bell } from "lucide-react";
+
+interface PropsHeader extends ComponentProps<"header"> {
+  title?: string;
+}
+
+export default function Header({ ...props }: PropsHeader) {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header
+      {...props}
+      className={`fixed top-0 left-0 w-full z-30 transition-colors duration-300 ${
+        isScrolled ? "bg-black" : "bg-transparent"
+      }`}
+    >
+      <div className="absolute top-0 left-0 w-full h-32 bg-gradient-to-b from-black/90 to-transparent z-10 pointer-events-none" />
+
+      <div className="relative flex items-center justify-between px-8 py-4 z-20">
+        <nav className="flex items-center">
+          <Image
+            src={Logo}
+            alt="Logotipo da Netflix"
+            width={100}
+            height={50}
+            className="mr-10"
+          />
+          <div className="gap-6 lg:gap-10 hidden md:flex text-white text-sm font-medium">
+            <p>Pagina Inicial</p>
+            <p>Series</p>
+            <p>Filmes</p>
+            <p>Jogos</p>
+            <p>Mais recentes</p>
+            <p>A minha lista</p>
+            <p>Procurar por idioma</p>
+          </div>
+        </nav>
+
+        <nav className="flex items-center gap-6 text-white">
+          <Search size={24} />
+          <Bell size={24} />
+          <Image
+            src={Profile}
+            alt="Imagem de Perfil da Netflix"
+            width={40}
+            height={40}
+            className="rounded"
+          />
+        </nav>
+      </div>
+    </header>
+  );
+}
